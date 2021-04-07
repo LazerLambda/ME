3b2ca99bc55a7bea6207982321d7cb7f8912782e
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-04-07 21:45:29 +0200

    KNneighbors class implemented, not yet testet

--- markevaluate/KNneighbors.py
+++ markevaluate/KNneighbors.py
@@ -1,15 +1,45 @@
-
+import collections
 import numpy as np
 
 
 class KNneighbors:
 
-    def __init__(self, embds : np.ndarray):
+    def __init__(self, embds : np.ndarray, k : int) -> None:
 
-        embds : np.ndarray = np.ndarray(list(set(embds)))
-        
-        self.dst_matrix = np.zeros((len(embds), len(embds)))
+        self.embds : np.ndarray = np.ndarray(list(set(self.embds)))
 
-        for i in embds:
-            for j in embds:
-                dist = np.linalg.norm(embds[i] - embds[j])
+        self.knns : np.ndarray = (np.zeros((len(self.embds), k)), np.zeros((len(self.embds), k)))
+        self.kmaxs : np.ndarray = np.zeros((len(self.embds), 1))
+        self.dst_matrix = np.zeros((len(self.embds), len(self.embds)))
+
+        for i in self.embds:
+
+            knns_index : collections.deque = collections.deque(range(k), maxlen=k)
+            knns_dist : collections.deque = collections.deque(range(k), maxlen=k)
+            kmax : float = 0
+
+            for j in self.embds:
+                dist : float = np.linalg.norm(self.embds[i] - self.embds[j])
+
+                if dist > knns_dist[(k - 1)]:
+                    knns_index.append(j)
+                    knns_dist.append(dist)
+
+                if dist > kmax:
+                    kmax = dist
+
+                self.embds[i][j] = dist
+
+            self.knns[0][i], self.knns[1][i] = np.asarray(knns_index), np.asarray(knns_dist)
+            self.kmaxs[i] = kmax
+
+
+
+    def get_knn(index : int) -> np.ndarray:
+        return self.knns[0][index], self.knns[1][index]
+
+
+
+    def is_in_hypersphere(index : int, sample : tuple) -> int:
+        dist : float = np.linalg.norm(self.embds[index] - sample)
+        return 1 if dist < self.kmaxs[index] else 0
