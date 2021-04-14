8bc1e204b9f5b237cb06b98219b5312df5744345
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-04-14 19:37:27 +0200

    Added new test, deleted test.ipynb

--- tests/test_markevaluate.py
+++ tests/test_markevaluate.py
@@ -12,32 +12,49 @@ from markevaluate import MarkEvaluate
 
 class TestMarkEvaluate(unittest.TestCase):
 
-    sentences = ['This framework provides an easy method to compute dense vector representations for sentences, paragraphs, and images.',\
+    sentences0 = ['This framework provides an easy method to compute dense vector representations for sentences0, paragraphs, and images.',\
         'This framework generates embeddings for each input sentence',\
-        'Sentences are passed as a list of string.',\
+        'sentences0 are passed as a list of string.',\
         'The quick brown fox jumps over the lazy dog.',\
         'This naming convention informs the test runner about which methods represent tests',\
         'The remainder of the documentation explores the full feature set from first principles.']
 
+    sentences1 = ["The move is expected to delay the country's vaccination programme by several weeks.",\
+        "Drug watchdog the European Medicines Agency last week announced a possible link with clots but said the risk of dying of Covid-19 was much greater.",\
+        "Several European countries had previously briefly suspended the jab.",
+        "Most have now resumed vaccinations with AstraZeneca, but often with limits to older age groups.",\
+        "South Africa has also paused its use, despite the Johnson & Johnson being its preferred vaccine because of its effectiveness against the South African variant",\
+        "Both vaccines work by a similar method, known as adenoviral vectors.",\
+        "Danish officials said that all 2.4 million doses of the AstraZeneca vaccine would be withdrawn until further notice."]
+
+    sentences2 = []
+
     def test_markevaluate1(self):
 
-        me = MarkEvaluate.MarkEvaluate(cand=self.sentences, ref=self.sentences)
+        me = MarkEvaluate.MarkEvaluate(cand=self.sentences0, ref=self.sentences0)
         self.assertEqual(me.estimate()['Peterson'], 1, msg="Test Peterson Estimator using Theorem A.1.")
     
     def test_markevaluate2(self):
 
-        me = MarkEvaluate.MarkEvaluate(cand=self.sentences, ref=self.sentences)
+        me = MarkEvaluate.MarkEvaluate(cand=self.sentences0, ref=self.sentences0)
         self.assertEqual(me.estimate()['Schnabel'], 1, msg="Test Schnabel Estimator using Theorem A.2.")
 
     def test_markevaluate3(self):
 
-        me = MarkEvaluate.MarkEvaluate(cand=self.sentences, ref=self.sentences, quality="")
+        me = MarkEvaluate.MarkEvaluate(cand=self.sentences0, ref=self.sentences0, quality="")
         self.assertEqual(me.estimate()['Schnabel'], 1, msg="Test Schnabel Estimator using Theorem A.2.")
 
     def test_markevaluate4(self):
 
-        me = MarkEvaluate.MarkEvaluate(cand=self.sentences, ref=self.sentences, quality="")
+        me = MarkEvaluate.MarkEvaluate(cand=self.sentences0, ref=self.sentences0)
         self.assertEqual(me.estimate()['CAPTURE'], 1, msg="Test CAPTURE Estimator using Theorem A.3.")
+
+    def test_markevaluate5(self):
+
+        me = MarkEvaluate.MarkEvaluate(cand=self.sentences0, ref=self.sentences1)
+        result = me.estimate()
+        print(result)
+        self.assertTrue(0 <= result['Schnabel'] and result['Schnabel'] <= 1, msg="Test different input with different topics and different lengths.")
         
 
 if __name__ == '__main__':
